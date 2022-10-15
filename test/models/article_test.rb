44ea4eb4b4545b7fab4e7e167febf0bc5168ee18
Nate Williams <natepaulwilliams@gmail.com>
2022-10-15 13:49:53 -0600

    Add test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "article attributes must not be empty" do
+    article = Article.new
+    assert article.invalid?
+    assert article.errors[:title].any?
+    assert article.errors[:description].any?
+    # assert article.errors[:image_url].any?
+  end
 end
